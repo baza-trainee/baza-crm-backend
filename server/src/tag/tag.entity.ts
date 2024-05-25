b566ac004c21e38dc443bc6a8865af4c066cdcac
pavloruban1 <pavloruban1@gmail.com>
2024-05-25 17:59:20 +0300

    InitTag

--- server/src/tag/tag.entity.ts
+++ server/src/tag/tag.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
 import { User } from '../user/user.entity';
 
+
 @Entity()
 export class Tag {
     @PrimaryGeneratedColumn()
