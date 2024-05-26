5c4c09354c3e31da1b25eb2fbad200b0691b9410
Anastasiia <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-05-26 15:00:10 +0200

    Feature/send email (#2)
    * add config(nodemailer)
    
    * add variables to env, text for letter
    
    * fix error handling
    
    * add loging errors
    
    ---------
    
    Co-authored-by: Ivan Khomenko <simkav2411@gmail.com>
    Co-authored-by: Ivan <46397494+Simkav@users.noreply.github.com>

---
+++ server/src/mail/mail.service.ts
@@ -0,0 +1,71 @@
+import nodemailer from 'nodemailer';
+import { ILetter } from './mail.types';
+
+const {
+  SMTP_PASSWORD,
+  SMTP_USER,
+  BASE_URL,
+  SMTP_PORT,
+  SMTP_SECURE,
+  SMTP_HOST,
+  SMTP_FROM,
+} = process.env;
+
+const nodemailerConfig = {
+  host: SMTP_HOST,
+  port: Number(SMTP_PORT),
+  secure: Boolean(SMTP_SECURE),
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+};
+
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async (letter: ILetter) =>
+  await transporter.sendMail({ ...letter, from: SMTP_FROM });
+
+export const sendVerificationEmail = async (email: string, link: string) => {
+  const letter = {
+    to: email,
+    subject: 'Verify email',
+    html: `<h2>Hi, ${email}.</h2>
+    <p>We just need to verify your email address before you can access.</p>
+    <p>Click below to verify your email address</p>
+    <a target="_blank" href="${BASE_URL}${link}">Click here</a> 
+    <p>Regards, Baza Trainee Ukraine</p>
+    <p>Thanks!</p>
+`,
+  };
+
+  try {
+    await sendEmail(letter);
+    console.log('success');
+  } catch (error) {
+    console.error(error);
+    throw new Error('Sorry, your message was not sent');
+  }
+};
+
+export const sendChangePWEmail = async (email: string, link: string) => {
+  const letter = {
+    to: email,
+    subject: 'Change password',
+    html: `<h2>Hi, ${email}.</h2>
+    <a target="_blank" href="${BASE_URL}${link}">Click here</a>
+    <p>Regards, Baza Trainee Ukraine</p>
+    <p>Thanks!</p>`,
+  };
+
+  try {
+    await sendEmail(letter);
+    console.log('success');
+  } catch (error) {
+    console.error(error);
+    throw new Error('Sorry, your message was not sent');
+  }
+};
