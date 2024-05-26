aefaa2bf77b74782d0989aca9500e5a9f32bc8e4
Ivan Khomenko <simkav2411@gmail.com>
2024-05-26 14:51:26 +0300

    fix user update schema

--- server/src/user/user.schemas.ts
+++ server/src/user/user.schemas.ts
@@ -7,5 +7,5 @@ export const getUserByIdParamSchema = Joi.object().keys({
 export const updateUserSchema = Joi.object().keys({
   linkedin: Joi.string().uri().optional(),
   name: Joi.string().optional(),
-  emailReceiving: Joi.string().email().optional(),
+  emailReceiving: Joi.boolean().optional(),
 });
